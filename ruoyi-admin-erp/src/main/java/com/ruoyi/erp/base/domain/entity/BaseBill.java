81b491e987e0cc55bffc027a2875ff7470c50933
zcc <zccbbg@qq.com>
2024-11-04 14:47:45 +0800

    feat: base bill 创建

---
+++ ruoyi-admin-erp/src/main/java/com/ruoyi/erp/base/domain/entity/BaseBill.java
@@ -0,0 +1,95 @@
+package com.ruoyi.erp.base.domain.entity;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.ruoyi.common.mybatis.core.domain.BaseEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class BaseBill extends BaseEntity {
+    /**
+     * id
+     */
+    @TableId(value = "id")
+    private Long id;
+    /**
+     * 单据编号
+     */
+    private String bizNo;
+    /**
+     * 审核状态
+     */
+    private Integer checkedStatus;
+    /**
+     * 审核人
+     */
+    private String checkName;
+
+    /**
+     * 单据日期
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDateTime billDate;
+    /**
+     * 往来单位id
+     */
+    private Long merchantId;
+    /**
+     * 商品数量
+     */
+    private BigDecimal goodsQuantity;
+
+    /**
+     * 已处理数量（入库单就是已入库数量，出库单就是已出库数量）
+     */
+    private BigDecimal processedQuantity;
+    /**
+     * 商品金额（未优惠）
+     */
+    private BigDecimal goodsAmount;
+
+    /**
+     * 其他费用金额
+     */
+    private BigDecimal otherExpensesAmount;
+    /**
+     * 备注
+     */
+    private String remark;
+
+    /**
+     * 交货日期
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDateTime deliveryDate;
+
+    /**
+     * 库存状态
+     */
+    private Integer stockStatus;
+
+
+    /**
+     * 优惠金额
+     */
+    private BigDecimal discountAmount;
+    /**
+     * 实际金额(货品金额+其他费用-优惠金额)
+     */
+    private BigDecimal actualAmount;
+
+    /**
+     * 已支付定金
+     */
+    private BigDecimal payedDepositAmount;
+
+    /**
+     * 已抵扣定金
+     */
+    private BigDecimal deductedDepositAmount;
+}
--- ruoyi-admin-erp/src/main/java/com/ruoyi/erp/purchase/domain/entity/PurchaseOrder.java
+++ ruoyi-admin-erp/src/main/java/com/ruoyi/erp/purchase/domain/entity/PurchaseOrder.java
@@ -1,11 +1,15 @@
 package com.ruoyi.erp.purchase.domain.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.ruoyi.erp.base.domain.entity.BaseBill;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.time.LocalDateTime;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 @TableName("purchase_order")
-public class PurchaseOrder {
+public class PurchaseOrder extends BaseBill {
 }
