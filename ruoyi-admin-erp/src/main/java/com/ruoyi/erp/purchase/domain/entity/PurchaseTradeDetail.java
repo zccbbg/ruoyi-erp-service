28a765127d8e6ae8de298bd7f9517b3ce182e72a
zcc <zccbbg@qq.com>
2025-02-07 14:41:35 +0800

    fix： 修改继承类关系

--- ruoyi-admin-erp/src/main/java/com/ruoyi/erp/purchase/domain/entity/PurchaseTradeDetail.java
+++ ruoyi-admin-erp/src/main/java/com/ruoyi/erp/purchase/domain/entity/PurchaseTradeDetail.java
@@ -2,11 +2,12 @@ package com.ruoyi.erp.purchase.domain.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.ruoyi.erp.base.domain.entity.BaseTrade;
+import com.ruoyi.erp.base.domain.entity.BaseTradeDetail;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 @TableName("purchase_receipt_trade")
-public class PurchaseTradeDetail extends BaseTrade {
+public class PurchaseTradeDetail extends BaseTradeDetail {
 }
