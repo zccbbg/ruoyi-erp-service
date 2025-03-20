e8e012e201c6000abf51bc6c3ddef005f0e44126
zcc <zccbbg@qq.com>
2025-03-20 14:32:30 +0800

    refactor(erp): 重构 BaseTradeVo 类
    - 移除了不必要的字段：
      - paymentStatus  - deductedRefundAmount - deductedPrepayAmount
      - dueAmount
    - 添加了新的字段：
      - orderNo
      - bankAccountId
    - 优化了类结构，提高了代码的可读性和维护性

--- ruoyi-admin-erp/src/main/java/com/ruoyi/erp/base/domain/vo/BaseTradeVo.java
+++ ruoyi-admin-erp/src/main/java/com/ruoyi/erp/base/domain/vo/BaseTradeVo.java
@@ -14,30 +14,15 @@ import java.util.List;
 @ExcelIgnoreUnannotated
 public class BaseTradeVo<T extends BaseTradeDetailVo> extends BaseBillVo<T> {
     private Long orderId;
-
-    private Integer paymentStatus;
+    private String orderNo;
 
     private Integer refundStatus;
 
     private BigDecimal refundAmount;
 
     /**
-     * 退款抵扣
-     */
-    private BigDecimal deductedRefundAmount;
-
-    /**
-     * 预付款抵扣
-     */
-    private BigDecimal deductedPrepayAmount;
-
-    /**
      * 支付金额
      */
     private BigDecimal paidAmount;
-
-    /**
-     * 剩余金额=实际金额-支付金额-退款抵扣-预付款抵扣
-     */
-    private BigDecimal dueAmount;
+    private Long bankAccountId;
 }
