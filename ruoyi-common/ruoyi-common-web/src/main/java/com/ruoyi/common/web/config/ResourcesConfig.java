032f8c5d46f53999108070750f187ebf08eaae46
zcc <zccbbg@qq.com>
2026-09-09 16:49:39 +0800

    refactor(web): 修改全局异常处理器以集成飞书异常通知功能
    - 添加 FeishuExceptionNotifier 依赖注入
    - 更新 GlobalExceptionHandler 构造函数参数
    - 实现异常信息向飞书推送的功能

--- ruoyi-common/ruoyi-common-web/src/main/java/com/ruoyi/common/web/config/ResourcesConfig.java
+++ ruoyi-common/ruoyi-common-web/src/main/java/com/ruoyi/common/web/config/ResourcesConfig.java
@@ -2,6 +2,7 @@ package com.ruoyi.common.web.config;
 
 import com.ruoyi.common.web.handler.GlobalExceptionHandler;
 import com.ruoyi.common.web.interceptor.PlusWebInvokeTimeInterceptor;
+import com.ruoyi.common.web.service.FeishuExceptionNotifier;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
@@ -55,7 +56,7 @@ public class ResourcesConfig implements WebMvcConfigurer {
      * 全局异常处理器
      */
     @Bean
-    public GlobalExceptionHandler globalExceptionHandler() {
-        return new GlobalExceptionHandler();
+    public GlobalExceptionHandler globalExceptionHandler(FeishuExceptionNotifier feishuExceptionNotifier) {
+        return new GlobalExceptionHandler(feishuExceptionNotifier);
     }
 }
